9fc475c21e1e4495869d6368637efc00d8e24139
Daniel Fabián <daniel.elbelaizi.alu@iescampanillas.com>
2021-11-12 13:49:54 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '';
+
+  ngOnInit() {
+    const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLElement>
+
+    buttons.forEach(button => {
+
+      button.addEventListener('click', function(event) {
+        AppComponent.hideEjercicios()
+  
+        const n = button.dataset.ej
+        const ejercicio = document.querySelector(`app-ejercicio${n}`)
+        ejercicio?.classList.add('show')
+      })
+
+    })
+  }
+
+
+  static hideEjercicios() {
+    const ejercicios = document.querySelectorAll('.ejercicio')
+    ejercicios.forEach(ej => ej.classList.remove('show'))
+  }
 }
